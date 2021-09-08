1ee3007ff99a30ac01b004b5119d3e936d086389
Benjamin Luengo Ackermann <benjaluengoa@gmail.com>
2021-09-08 13:33:14 -0300

    Se agrega componente About Us y linkeo de rutas

--- Trabajos_prácticos/TP6/Producto/Frontend/src/app/app-routing.module.ts
+++ Trabajos_prácticos/TP6/Producto/Frontend/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
 import { PedidoComponent } from './pedido/pedido.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/pedido', pathMatch: 'full'},
-  { path: 'pedido', component: PedidoComponent}
+  //{ path: '', redirectTo: '/pedido', pathMatch: 'full'},
+  { path: 'pedido', component: PedidoComponent},
+  { path: 'about-us', component: AboutUsComponent}
 ];
 
 @NgModule({
--- Trabajos_prácticos/TP6/Producto/Frontend/src/app/app.module.ts
+++ Trabajos_prácticos/TP6/Producto/Frontend/src/app/app.module.ts
@@ -1,22 +1,31 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { PedidoComponent } from './pedido/pedido.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    PedidoComponent
+    PedidoComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: '/pedido', pathMatch: 'full' },
+      { path: 'pedido', component: PedidoComponent },
+      { path: 'about-us', component: AboutUsComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
