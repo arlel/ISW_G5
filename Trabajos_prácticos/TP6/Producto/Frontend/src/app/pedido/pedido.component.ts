0ec633cdf6f185e3f4f20b820a5dff4194095dd6
Benjamin Luengo Ackermann <benjaluengoa@gmail.com>
2021-09-08 23:27:52 -0300

    Se agregan comentarios acerca de las restricciones

--- Trabajos_prácticos/TP6/Producto/Frontend/src/app/pedido/pedido.component.ts
+++ Trabajos_prácticos/TP6/Producto/Frontend/src/app/pedido/pedido.component.ts
@@ -11,15 +11,21 @@ export class PedidoComponent implements OnInit {
   Titulo = "Nuevo Pedido";
 
   FormPedido: FormGroup;
-
-
+  //   La foto debe tener un maximo de 5MB
+  // Ciudades que se pueden elegir son CBA Capital, VCP y Rio Primero
+  // Descripcion del pedido min 5 char max 255 char
+  // Minimo 1 hs despues, maximo 1 semana despues para la entrega del pedido
+  // Horario de trabajo 8 a 00 lunes a lunes
+  // Direccion de COMERCIO en forma textual* o seleccionando punto en el mapa
+  // Direccion de ENTREGA en forma textual*
+    // * forma textual = calle, numero, ciudad y referencia opcional en formato texto 
   constructor(
     public formBuilder: FormBuilder
   ) {}
 
   ngOnInit(): void {
     this.FormPedido = this.formBuilder.group({
-      Descripcion: [null],
+      Descripcion: [null], //minimo 5 caracteres maximo 255
       Domicilio: [null],
       Comercio: [null]
 
