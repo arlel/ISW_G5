37310ee0b2c05492c29aef1841b5c006594ef8b4
seseseflip <sebastianlesze@gmail.com>
2021-09-11 19:58:15 -0300

    algunas validadicones
    se agregaron validaciones al gesto de pedidos

--- Trabajos_prácticos/TP6/Producto/Backend/Logica_negocio/Gestor_Pedidos.py
+++ Trabajos_prácticos/TP6/Producto/Backend/Logica_negocio/Gestor_Pedidos.py
@@ -1,3 +1,7 @@
+from datetime import date
+from datetime import datetime
+from datetime import timedelta
+
 from Entidades import Pedido
 
 
@@ -11,5 +15,42 @@ class Gestor_pedidos:
         monto = json["monto_total"]
         fh = json["fecha_hora"]
         # validaciones
+
+    def validar_desc(desc):
+        if desc.length < 5:
+            print("¡La descripción debe tener como mínimo 5 carácteres!")
+            return
+        elif desc.leghth > 240:
+            print("¡La descripción debe tener como máximo 240 carácteres!")
+            return
+
+    def validar_imagen(img):
+        return
+
+    def validar_comercio(com):
+        return
+
+    def validar_direccion(dire):
+        return
+
+    def validar_efectivo(efectivo):
+        if efectivo == True:
+            def validar_monto(monto):
+                if monto != float:
+                    print('Ingrese el monto a pagar con números')
+                    return
+        else:
+            return
+
+
+    def validar_fecha(fh):
+        now = datetime.now()
+        if fh.datetime <= now + timedelta(minutes=59) and fh.datetime.day == datetime.today():
+            print("El pedido debe ser entregado como mínimo dentro de 1 hora")
+            return
+        elif fh.datetime > now + timedelta(days=7):
+            print("El pedido debe ser entregado como máximo en una semana")
+            return
+
         pedido = Pedido(desc, img, com, dire, efectivo, monto, fh)
         return pedido.get_id()
