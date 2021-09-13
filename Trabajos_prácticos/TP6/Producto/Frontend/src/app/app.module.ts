ef34ba0f8cb7e1a80e9553ae4a8548ce817dd600
Benjamin Luengo Ackermann <benjaluengoa@gmail.com>
2021-09-13 01:45:16 -0300

    Integracion GMaps, Validaciones y Pago

--- Trabajos_prácticos/TP6/Producto/Frontend/src/app/app.module.ts
+++ Trabajos_prácticos/TP6/Producto/Frontend/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-
+import { AgmCoreModule } from '@agm/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,18 +10,24 @@ import { MenuComponent } from './menu/menu.component';
 import { PedidoComponent } from './pedido/pedido.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { GMapsComponent } from './g-maps/g-maps.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     PedidoComponent,
-    AboutUsComponent
+    AboutUsComponent,
+    GMapsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyDUpJIfYIGj2XYFZsmqdyQygHStR0cPuuA',
+      libraries: ['places']
+    }),
     CommonModule,
     RouterModule.forRoot([
       { path: '', redirectTo: '/pedido', pathMatch: 'full' },
--- Trabajos_prácticos/TP6/Producto/Frontend/src/app/pedido/pedido.component.ts
+++ Trabajos_prácticos/TP6/Producto/Frontend/src/app/pedido/pedido.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Alert } from 'selenium-webdriver';
+import { Direccion } from '../models/direccion';
 import { Pedido } from '../models/pedido';
 
 @Component({
@@ -10,13 +12,25 @@ import { Pedido } from '../models/pedido';
 export class PedidoComponent implements OnInit {
 
   Titulo = "Nuevo Pedido";
+  Direccion = new Direccion();
   Pedidos = [];
   Accion = "N" //la accion inicial es agregar un nuevo pedido
   Opcion = "";
+  Entrega = ""; // Para elegir la fecha con un datetime o elegir que sea inmediata
+  FormaIngresoDomicilio = ""; // Para elegir la fecha con un datetime o elegir que sea inmediata
+
+
+
+  LoAntesPosible = false;
+  submitted = false;
+  submittedPago = false;
+
 
   FormPedido: FormGroup;
   FormCarrito: FormGroup;
   FormPago: FormGroup;
+  FormPagoEfectivo:  FormGroup;
+  FormPagoTarjeta: FormGroup;
   //   La foto debe tener un maximo de 5MB
   // Ciudades que se pueden elegir son CBA Capital, VCP y Rio Primero
   // Descripcion del pedido min 5 char max 255 char
@@ -31,29 +45,42 @@ export class PedidoComponent implements OnInit {
 
   ngOnInit(): void {
     this.FormPedido = this.formBuilder.group({
-      Descripcion: [null], //minimo 5 caracteres maximo 255
-      Domicilio: [null],
-      Comercio: [null]
+      Descripcion: [null, [Validators.required, Validators.minLength(5), Validators.maxLength(240)]], //minimo 5 caracteres maximo 255
+      Domicilio: [null,  [Validators.required, Validators.minLength(5), Validators.maxLength(240)]],
+      Comercio: [null,  [Validators.required, Validators.minLength(5), Validators.maxLength(240)]] 
 
     });
 
     this.FormCarrito = this.formBuilder.group({
-      Descripcion: [null], //minimo 5 caracteres maximo 255
-      Domicilio: [null],
-      Comercio: [null]
+      Descripcion: [null, [Validators.required, Validators.minLength(5), Validators.maxLength(240)]], //minimo 5 caracteres maximo 255
+      Domicilio: [null,  [Validators.required, Validators.minLength(5), Validators.maxLength(240)]],
+      Comercio: [null,  [Validators.required, Validators.minLength(5), Validators.maxLength(240)]] 
 
     });
 
     this.FormPago = this.formBuilder.group({
-      Monto: [null],
-      Tarjeta: [null],
-      CVV:[null],
-      Venc:[null],
-      Titular:[null]
+    });
+
+    this.FormPagoEfectivo = this.formBuilder.group({
+      Monto: [null,  [Validators.required, Validators.pattern('[0-9]{1,10}')]]
+    });
+
+    this.FormPagoTarjeta = this.formBuilder.group({
+      Tarjeta: [null,  [Validators.required,  Validators.pattern("^4[0-9]{12}(?:[0-9]{3})?$")]],
+      CVV:[null, [Validators.required, Validators.pattern('[0-9]{3}')]],
+      Venc:[null, [Validators.required, Validators.pattern("(0[1-9]|1[012])[-/](19|20)[0-9]{2}")]],
+      Titular:[null, [Validators.required, Validators.minLength(5), Validators.maxLength(24)]]
     });
   }
 
+  ValorAntesPosible(){
+    this.LoAntesPosible = !this.LoAntesPosible;
+  }
   AgregarPedido(){
+    if (this.FormPedido.invalid) {
+      alert("No puede proceder al carrito debido a que existen errores")
+      return;
+    }
     this.Accion = 'C';
     var nuevoPed = new Pedido();
     nuevoPed.Domicilio = this.FormPedido.value.Domicilio;
@@ -61,24 +88,98 @@ export class PedidoComponent implements OnInit {
     nuevoPed.Comercio = this.FormPedido.value.Comercio;
     this.Pedidos.push(nuevoPed);
     this.FormCarrito.reset();
+    this.submitted = true;
+    this.FormPedido.markAsUntouched(); 
   }
 
   AgregarOtro(){
     this.Accion ="N";
     this.FormPedido.reset();
+    this.FormPedido.markAsUntouched(); 
+    this.submitted = false;
+
   }
 
   ProcederAPago(){
     this.Accion = "P";
     this.FormPago.reset();
+    this.FormPago.markAsUntouched();
   }
 
   SelecFormaPago(opcion){
     this.Opcion = opcion;
   }
 
+  SelecFormaEntrega(opcion){
+    this.Entrega = opcion;
+  }
+
+  SelecFormaIngresoDomicilio(opcion){
+    this.FormaIngresoDomicilio = opcion;
+
+  }
+
   Pagar(){
-    //WIP
+    if (this.FormPago.invalid) {
+      alert("No puede proceder al pago debido a que existen errores")
+      return;
+    }
+
+    if (this.Opcion == "E") this.PagarEfectivo();
+    else this.PagarTarjeta();
+
+   
+  }
+
+    PagarEfectivo(){
+      if (this.FormPagoEfectivo.invalid) {
+        alert("No puede proceder al pago debido a que existen errores")
+        return;
+      }
+      this.FormPagoEfectivo.reset();
+      this.submittedPago = true;
+      this.FormPagoEfectivo.markAsUntouched();
+      alert("El pedido se ha realizado con exito");
+      this.Volver();
+    }
+    PagarTarjeta(){
+      if (this.FormPagoTarjeta.invalid) {
+      alert("No puede proceder al pago debido a que existen errores")
+      return;
+    }
+    this.FormPagoTarjeta.reset();
+    this.submittedPago = true;
+    this.FormPagoTarjeta.markAsUntouched();
+    alert("El pedido se ha realizado con exito y el pago se ha procesado")
+    this.Volver();
+  }
+
+  Volver(){
+    this.FormPago.reset();
+    this.FormPedido.reset();
+    this.FormCarrito.reset();
+    this.FormPagoEfectivo.reset();
+    this.FormPagoTarjeta.reset();
+    this.FormPago.markAsUntouched();   
+    this.FormPedido.markAsUntouched();   
+    this.FormCarrito.markAsUntouched();
+    this.FormPagoEfectivo.markAsUntouched();
+    this.FormPagoTarjeta.markAsUntouched();
+
+    this.submittedPago = false;
+    this.submitted = false;
+
+    this.Pedidos = [];
+
+    this.Accion = "N"
+    this.Opcion = "";
+    this.Entrega = "";
+    this.FormaIngresoDomicilio = "";
+
+
+
+
+
   }
 
 }
