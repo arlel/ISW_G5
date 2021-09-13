8767721cbdac1a175b4ec439995f5e19a085a113
arlel <lenta.ariel@gmail.com>
2021-09-13 20:15:12 -0300

    Correcciones a las validaciones

--- Trabajos_prácticos/TP6/Producto/Backend/Logica_negocio/Gestor_Pedidos.py
+++ Trabajos_prácticos/TP6/Producto/Backend/Logica_negocio/Gestor_Pedidos.py
@@ -1,20 +1,19 @@
-from datetime import date
-from datetime import datetime
-from datetime import timedelta
+from datetime import datetime, timedelta, time
 
-from Entidades import Pedido
+from Entidades import Pedido, Direccion
 
 
 class Gestor_pedidos:
     desc = None
     img = None
     com = None
-    dire = None
+    dire = Direccion.Direccion(None, None, None, None)
     efectivo = None
     monto = None
     fh = None
 
     def crear_pedido(self, json):
+        print(json)
         self.desc = json["descripcion"]
         self.img = json["imagen"]
         self.com = json["comercio"]
@@ -23,21 +22,23 @@ class Gestor_pedidos:
         self.monto = json["monto_total"]
         self.fh = json["fecha_hora"]
         # convertir el json a un datetime que lo tome bien, es decir separar
-        self.fh = datetime(self.fh[0:1])
+        # self.fh = datetime(self.fh[0:1])
         # validaciones
-
-        # si salio bien retorna un positivo, sino negativo
-        pedido = Pedido(self.desc, self.img, self.com, self.dire,
-                        self.efectivo, self.monto, self.fh)
-        return pedido.get_id()
+        if self.validar_desc() and self.validar_monto() \
+                and self.validar_fecha():
+            # si salio bien retorna un positivo, sino negativo
+            pedido = Pedido.Pedido(self.desc, self.img, self.com, self.dire,
+                                   self.efectivo, self.monto, self.fh)
+            return pedido.get_id()
+        return -1
 
     def validar_desc(self):
         if self.desc.length < 5:
-            print("¡La descripción debe tener como mínimo 5 carácteres!")
-            return
+            # print("¡La descripción debe tener como mínimo 5 carácteres!")
+            return False
         elif self.desc.leghth > 240:
-            print("¡La descripción debe tener como máximo 240 carácteres!")
-            return
+            # print("¡La descripción debe tener como máximo 240 carácteres!")
+            return False
 
     def validar_imagen(self):
         return
@@ -49,13 +50,9 @@ class Gestor_pedidos:
         return
 
     def validar_efectivo(self):
+        return
 
-        if self.efectivo:
-            return
-        else:
-            return
-
-# Valida que sea positivo el valor del monto, si es positivo, retorna true
+    # Valida que sea positivo el valor del monto, si es positivo, retorna true
     def validar_monto(self):
         if self.monto >= 0:
             return True
@@ -63,22 +60,16 @@ class Gestor_pedidos:
             return False
 
     def validar_fecha(self):
+        print(self.fh)
         now = datetime.now()
-        if self.fh <= now:
+        # si el pedido es para antes de ahora
+        if self.fh <= now + timedelta(minutes=59):
             return False
-
-        if now.date() == self.fh.date():
-            if now.time() <= self.fh.time():
-                return
-
-
-'''        
-        if self.fh.datetime <= now + timedelta(minutes=59) and\
-                self.fh.datetime.day == datetime.today():
-            print("El pedido debe ser entregado como mínimo dentro de 1 hora")
-            return
-        elif self.fh.datetime > now + timedelta(days=7):
-            print("El pedido debe ser entregado como máximo en una semana")
-            return
-'''
-
+        # si la fecha_y_hora de de entrega es mayor a 1 semana
+        if self.fh > now + timedelta(days=7):
+            return False
+        minimo = time(hour=8)
+        # maximo = time.max()
+        if self.fh.time() < minimo or self.fh.time != time(0):
+            return False
+        return True
